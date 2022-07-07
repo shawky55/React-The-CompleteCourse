cdbce2db9aa1a7ef04fe550df776166b12f6b924
shawky  sobhy <shawkysobhy98@gmail.com>
2022-07-07 10:55:45 +0200

    use custom hooks

--- section16/myApp/src/components/SimpleInput.js
+++ section16/myApp/src/components/SimpleInput.js
@@ -1,71 +1,95 @@
 import react, { useRef, useState } from 'react';
+import useInput from '../customHooks/useInput';
 const SimpleInput = (props) => {
   // let inputValue = useRef('');
   // const [enterdNameValid, setEnteredNameValid] = useState(false);
-  const [name, setEnteredName] = useState('');
-  const [enterdNameTouched, setEnteredNameTouched] = useState(false);
-  const enterdNameValid = name.trim() !== '';
-  //////emali
-  const [email, setEnteredEmail] = useState('');
-  const [enterdEmailTouched, setEnteredEmailTouched] = useState(false);
-  function validateEmail(email) {
+  // const [name, setEnteredName] = useState('');
+  // const [enterdNameTouched, setEnteredNameTouched] = useState(false);
+  // const enterdNameValid = name.trim() !== '';
+  const nameInputValidation = (inputValue) => {
+    return inputValue.trim() !== '';
+  };
+  const validateEmail = (email) => {
     const re =
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
-  }
-  const enteredEmailValid = validateEmail(email);
-  console.log('out of funciton', enteredEmailValid);
-  let inputEmailisInValid = !enteredEmailValid && enterdEmailTouched;
-  let inputNameIsInValid = !enterdNameValid && enterdNameTouched;
-  let formIsValid = false;
+  };
+  const {
+    value: nameInputValue,
+    inputChangeHandler: nameChangeHandler,
+    inputBlurHandler: nameBlurHandler,
+    inputError: nameInputError,
+    resetInput: nameResetInput,
+    inputIsValid: nameIsValid,
+  } = useInput(nameInputValidation);
+  //////emali
+  const {
+    value: emailInputValue,
+    inputChangeHandler: emailChangeHandler,
+    inputBlurHandler: emailBlurHandler,
+    inputError: emailInputError,
+    resetInput: emailResetInput,
+    inputIsValid: emailIsValid,
+  } = useInput(validateEmail);
+  // const [email, setEnteredEmail] = useState('');
+  // const [enterdEmailTouched, setEnteredEmailTouched] = useState(false);
 
-  if (enterdNameValid && enteredEmailValid) {
+  // const enteredEmailValid = validateEmail(email);
+  // console.log('out of funciton', enteredEmailValid);
+  // let inputEmailisInValid = !enteredEmailValid && enterdEmailTouched;
+  // let inputNameIsInValid = !enterdNameValid && enterdNameTouched;
+
+  let formIsValid = false;
+  if (nameIsValid && emailIsValid) {
     formIsValid = true;
   }
-  function inputNameHandler(e) {
-    setEnteredNameTouched(true);
-    setEnteredName(e.target.value);
-    // if (e.target.value.trim() !== '') {
-    //   setEnteredNameValid(true);
-    // }
-  }
-  function inputEmailHandler(e) {
-    setEnteredEmail(e.target.value);
-  }
+  // function inputNameHandler(e) {
+  //   setEnteredNameTouched(true);
+  //   setEnteredName(e.target.value);
+  //   // if (e.target.value.trim() !== '') {
+  //   //   setEnteredNameValid(true);
+  //   // }
+  // }
+  // function inputEmailHandler(e) {
+  //   setEnteredEmail(e.target.value);
+  // }
 
-  function inputNameBlurHandler(e) {
-    setEnteredNameTouched(true);
-    setEnteredName(e.target.value);
-    // if (name.trim() === '') {
-    //   setEnteredNameValid(false);
-    //   return;
-    // }
-  }
-  function inputEmailBlurHandler(e) {
-    setEnteredEmailTouched(true);
-    setEnteredEmail(e.target.value);
-  }
+  // function inputNameBlurHandler(e) {
+  //   setEnteredNameTouched(true);
+  //   setEnteredName(e.target.value);
+  //   // if (name.trim() === '') {
+  //   //   setEnteredNameValid(false);
+  //   //   return;
+  //   // }
+  // }
+  // function inputEmailBlurHandler(e) {
+  //   setEnteredEmailTouched(true);
+  //   setEnteredEmail(e.target.value);
+  // }
   function formSubmitHandler(e) {
     e.preventDefault();
-    setEnteredNameTouched(true);
-    if (!enterdNameValid && !enteredEmailValid) {
+    // setEnteredNameTouched(true);
+
+    if (!nameIsValid && !emailIsValid) {
       // setEnteredNameValid(false);
       return;
     }
     // setEnteredNameValid(true);
-    console.log(name);
-    console.log(email);
-    setEnteredName('');
-    setEnteredEmail('');
-    setEnteredNameTouched(false);
-    setEnteredEmailTouched(false);
+    console.log(nameInputValue);
+    console.log(emailInputValue);
+    nameResetInput();
+    emailResetInput();
+    // setEnteredName('');
+    // setEnteredEmail('');
+    // setEnteredNameTouched(false);
+    // setEnteredEmailTouched(false);
     // console.log(inputValue.current.value);
   }
-  let inputNameStyle = !inputNameIsInValid
+  let inputNameStyle = !nameInputError
     ? 'form-control'
     : 'form-control invalid';
 
-  let emailClassStyle = !inputEmailisInValid
+  let emailClassStyle = !emailInputError
     ? 'form-control'
     : 'form-control invalid';
   return (
@@ -75,14 +99,14 @@ const SimpleInput = (props) => {
         <input
           type="text"
           id="name"
-          value={name}
-          onChange={inputNameHandler}
-          onBlur={inputNameBlurHandler}
+          value={nameInputValue}
+          onChange={nameChangeHandler}
+          onBlur={nameBlurHandler}
 
           // ref={inputValue}
         />
       </div>
-      {inputNameIsInValid && (
+      {nameInputError && (
         <p className="error-text">input not Valid,try again!</p>
       )}
       <div className={emailClassStyle}>
@@ -90,12 +114,12 @@ const SimpleInput = (props) => {
         <input
           type="text"
           id="email"
-          value={email}
-          onChange={inputEmailHandler}
-          onBlur={inputEmailBlurHandler}
+          value={emailInputValue}
+          onChange={emailChangeHandler}
+          onBlur={emailBlurHandler}
         />
       </div>
-      {inputEmailisInValid && (
+      {emailInputError && (
         <p className="error-text">input not Valid,try again!</p>
       )}
       <div className="form-actions">
---
+++ section16/myApp/src/customHooks/useInput.js
@@ -0,0 +1,33 @@
+import react, { useState } from 'react';
+
+const useInput = (inputValidation) => {
+  const [enterdValue, setEnteredValue] = useState('');
+  const [inputTouched, setInputTouched] = useState('');
+  const inputIsValid = inputValidation(enterdValue);
+  const inputError = !inputIsValid && inputTouched;
+
+  const inputChangeHandler = (event) => {
+    setInputTouched(true);
+    setEnteredValue(event.target.value);
+  };
+  const inputBlurHandler = (event) => {
+    setInputTouched(true);
+    setEnteredValue(event.target.value);
+  };
+
+  const resetInput = () => {
+    setEnteredValue('');
+    setInputTouched('');
+  };
+
+  return {
+    value: enterdValue,
+    inputIsValid,
+    inputError,
+    inputChangeHandler,
+    inputBlurHandler,
+    resetInput,
+  };
+};
+
+export default useInput;
